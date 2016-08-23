8704da81300c1b7c2cacf98426c93a06c9d612ad
HSOOJ <blue8959@naver.com>
2016-08-23 16:08:52 +0900

    Add Order.java

---
+++ TinkerBell/src/Order.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class Order {
+	public static final HashMap<Integer, Integer> menu_board = new HashMap<Integer, Integer>();
+	
+	public int table_num = -1;
+	public int[] menu_code;
+	public int order_num;
+}
