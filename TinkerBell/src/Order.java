4c765bc44366e2681ab2d52fbb48da0ebd0bfc6a
HSOOJ <blue8959@naver.com>
2016-08-26 23:32:46 +0900

    AddOrder.java

--- TinkerBell/src/Order.java
+++ TinkerBell/src/Order.java
@@ -1,9 +1,15 @@
-import java.util.HashMap;
 
 public class Order {
-	public static final HashMap<Integer, Integer> menu_board = new HashMap<Integer, Integer>();
-	
+		
+	public int order_num;
 	public int table_num = -1;
 	public int[] menu_code;
-	public int order_num;
+	public static int currentOrderNumber = -1;
+	
+	public Order(int table_num, int[] menu_code) {
+		this.order_num = currentOrderNumber;
+		currentOrderNumber++;
+		this.table_num = table_num;
+		this.menu_code = menu_code;
+	}
 }
---
+++ TinkerBell/src/Server.java
@@ -0,0 +1,7 @@
+public class Server {
+	
+	public void MakeOrder(int table_num, int[] menu_code){//손님으로부터 주문받고 주문만들고 보내기를 해야함
+	//	Order o = new Order(table_num, menu_code);//주문받고 주문 만들기
+	}
+	
+}
