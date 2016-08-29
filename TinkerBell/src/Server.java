d151083060d65414b49c5e848b38ef24b565dc63
HSOOJ <blue8959@naver.com>
2016-08-29 13:55:42 +0900

    Add server

--- TinkerBell/src/Server.java
+++ TinkerBell/src/Server.java
@@ -2,7 +2,7 @@ public class Server {
 	
 	public void MakeOrder(int table_num, int[] menu_code){//손님으로부터 주문받고 주문만들고 보내기를 해야함
 		Order o = new Order(table_num, menu_code);//주문받고 주문 만들기
-		// Counter.takeOrder(o);
+		// Counter.takeOrder(o); 카운터로 주문보내기
 	}
 	
 }
